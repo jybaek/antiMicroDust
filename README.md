5f6f7f8bf0f8a9c29b8251383b6c327132a5e201
jybaek <oops.jybaek@gmail.com>
2018-09-11 08:49:02 +0900

    Account information setting to refer to file   - Update README.md   - Update .gitignore   - Add requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # antiMicroDust
+Imports micro-dust information from public data and sends it to specific users (friends) on Facebook.
+
+## Install
+Install the necessary modules.
+```bash
+$ pip install -r requirements.txt
+```
+
+## Usage
+Now modify _config.ini.default_ to make it _config.ini_ and run the script.
+```bash
+$ python antiMicroDust.py
+```
--- antiMicroDust.py
+++ antiMicroDust.py
@@ -1,11 +1,23 @@
 # encoding=utf-8
 
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+if not config['ACCOUNT']['USERID']:
+    print('Error: USERID in config.ini')
+    exit(255)
+if not config['ACCOUNT']['PASSWD']:
+    print('Error: USERID in config.ini')
+    exit(255)
+
 from fbchat import Client
 from fbchat.models import *
 
-fb_client = Client('email', 'password') # FIXME.
+fb_client = Client(config['ACCOUNT']['USERID'], config['ACCOUNT']['PASSWD'])
 
-friends = fb_client.searchForUsers('friendName')[0] # XXX.
+friends = fb_client.searchForUsers(config['TARGET']['USERNAME'])[0]
 msg = 'sample message' # XXX.
 
 fb_client.sendMessage(msg, thread_id=friends.uid, thread_type=ThreadType.USER)
