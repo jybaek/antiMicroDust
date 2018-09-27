e5431bd34e94c35b2c2980c4b94e72156878f812
jybaek <oops.jybaek@gmail.com>
2018-09-28 08:39:20 +0900

    Function Modularization

--- antiMicroDust.py
+++ antiMicroDust.py
@@ -2,8 +2,18 @@
 
 import configparser
 
-config = configparser.ConfigParser()
+config = configparser.RawConfigParser()
 config.read('config.ini')
+
+import dust_info
+
+data = dust_info.get_data(config)
+
+'''
+print("dataTime: ", data['dataTime'])
+print("pm10    : ", data['pm10Value'])
+print("pm2.5   : ", data['pm25Value'])
+'''
 
 if not config['ACCOUNT']['USERID']:
     print('Error: USERID in config.ini')
@@ -18,7 +28,9 @@ from fbchat.models import *
 fb_client = Client(config['ACCOUNT']['USERID'], config['ACCOUNT']['PASSWD'])
 
 friends = fb_client.searchForUsers(config['TARGET']['USERNAME'])[0]
-msg = 'sample message' # XXX.
+msg = "dataTime: %s\npm10    : %s\npm2.5   : %s" % ( data['dataTime'], data['pm10Value'], data['pm25Value'] )
+print(msg)
+exit(255)
 
 fb_client.sendMessage(msg, thread_id=friends.uid, thread_type=ThreadType.USER)
 fb_client.logout()
--- dust_info.py
+++ dust_info.py
@@ -4,17 +4,16 @@ import requests
 import urllib.parse
 import json
 
-url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty'
-service_key = '' # FIXME
-stationName = '강남구' # FIXME
+def get_data(config):
+    url = config['DATA']['URL']
+    service_key = config['DATA']['SERVICE_KEY']
+    stationName = config['DATA']['STATION_NAME']
 
-decode_key = urllib.parse.unquote(service_key)
-params = {'ServiceKey': decode_key, 'stationName': stationName, 'dataTerm': 'daily', '_returnType': 'json', 'ver': '1.3', 'pageNo': 1}
+    decode_key = urllib.parse.unquote(service_key)
+    params = {'ServiceKey': decode_key, 'stationName': stationName, 'dataTerm': 'daily', '_returnType': 'json', 'ver': '1.3', 'pageNo': 1}
 
-response = requests.get(url, params=params)
-response_body = json.loads(response.text)
+    response = requests.get(url, params=params)
+    response_body = json.loads(response.text)
 
-print(response.url)
-print("dataTime: ", response_body['list'][0]['dataTime'])
-print("pm10    : ", response_body['list'][0]['pm10Value'])
-print("pm2.5   : ", response_body['list'][0]['pm25Value'])
+    #print(response.url)
+    return response_body['list'][0]
